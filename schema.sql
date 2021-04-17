6fd876ffccc0f2f30b62b66b07448729c9518de7
petercolella <peter.joseph.colella@gmail.com>
2021-04-17 15:56:20 -0400

    adds users table to schema.sql

--- schema.sql
+++ schema.sql
@@ -5,8 +5,17 @@ CREATE DATABASE great_bay;
 USE great_bay;
 
 CREATE TABLE items (
-  id INT NOT NULL AUTO_INCREMENT,
-  item_description VARCHAR(45) NOT NULL,
-  highest_bid DECIMAL(10,2) NOT NULL,
-  PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    item_description VARCHAR(45) NOT NULL,
+    highest_bid DECIMAL(10, 2) NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(45) NOT NULL,
+    user_password VARCHAR(45) NOT NULL,
+    is_admin BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
