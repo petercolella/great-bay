688d15e1e850b85e7e12f19c0231b75a7400fa4e
petercolella <peter.joseph.colella@gmail.com>
2021-04-19 15:26:31 -0400

    adds administrator menu

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const init = () => {
       {
         type: "list",
         name: "init",
-        choices: ["View BID Menu", "View POST menu", "QUIT"],
+        choices: ["View BID Menu", "View POST menu", "View ADMIN menu", "QUIT"],
         message: "What would you like to do?",
       },
     ])
@@ -92,6 +92,9 @@ const init = () => {
           break;
         case "View POST menu":
           postMenu();
+          break;
+        case "View ADMIN menu":
+          adminMenu();
           break;
         default:
           connection.end();
@@ -425,6 +428,126 @@ const postMenu = () => {
           postMenu();
       }
     });
+};
+
+const adminAuctionChoices = async () => {
+  const rows = await connection.promisifiedQuery(
+    "SELECT id, item_description FROM auctions"
+  );
+
+  return rows.map((row) => {
+    return {
+      name: `${row.id} ${row.item_description}`,
+      value: row.id,
+    };
+  });
+};
+
+const adminUserChoices = async () => {
+  const rows = await connection.promisifiedQuery(
+    "SELECT id, username FROM users"
+  );
+
+  return rows.map((row) => {
+    return {
+      name: `${row.id} ${row.username}`,
+      value: row.id,
+    };
+  });
+};
+
+const adminMenu = async () => {
+  const auctionRowChoiceArray = await adminAuctionChoices();
+  const userRowChoiceArray = await adminUserChoices();
+
+  connection.query(
+    "SELECT username, is_admin FROM users WHERE id = ?",
+    [currentUserId],
+    (err, data) => {
+      if (err) throw err;
+
+      const { username, is_admin } = data[0];
+
+      if (is_admin) {
+        console.log(`\nWelcome, ${username}\n`);
+
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "table",
+              choices: ["auctions", "users"],
+              message: "Which table would you like to modify?",
+            },
+            {
+              type: "list",
+              name: "row",
+              choices: auctionRowChoiceArray,
+              message: "Which row would you like to modify?",
+              when: ({ table }) => table === "auctions",
+            },
+            {
+              type: "list",
+              name: "row",
+              choices: userRowChoiceArray,
+              message: "Which row would you like to modify?",
+              when: ({ table }) => table === "users",
+            },
+            {
+              type: "list",
+              name: "column",
+              choices: [
+                "item_description",
+                "starting_bid",
+                "highest_bid",
+                "is_auction_open",
+                "created_by_id",
+                "highest_bidder_id",
+              ],
+              message: "Which column would you like to modify?",
+              when: ({ table }) => table === "auctions",
+            },
+            {
+              type: "list",
+              name: "column",
+              choices: ["username", "user_password", "is_admin"],
+              message: "Which column would you like to modify?",
+              when: ({ table }) => table === "users",
+            },
+            {
+              type: "input",
+              name: "value",
+              message: "What is the new value?",
+            },
+          ])
+          .then(({ table, row, column, value }) => {
+            connection.query(
+              "UPDATE ?? SET ? WHERE ?",
+              [
+                table,
+                {
+                  [column]: value,
+                },
+                {
+                  id: row,
+                },
+              ],
+              (err, res) => {
+                if (err) throw err;
+
+                console.log(
+                  `\nThe ${column} has been updated to ${value} on row ${row} of the ${table} table.\n`
+                );
+                init();
+              }
+            );
+          });
+      } else {
+        console.log(`\nSorry ${username}, you're not an administrator.\n`);
+        init();
+      }
+    }
+  );
 };
 
 connection.connect((err) => {
