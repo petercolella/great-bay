c9e0eaa7002b3b569b43baf1d925875b965867ee
petercolella <peter.joseph.colella@gmail.com>
2021-04-19 09:24:17 -0400

    updates index.js with promisified connection query method, and new functions: getUserAuctionArray, closeAuction, editAuction, viewAuctions, and postMenu

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const util = require("util");
 
 const connection = mysql.createConnection({
   port: 3306,
@@ -8,6 +9,7 @@ const connection = mysql.createConnection({
   database: "great_bay",
 });
 
+connection.promisifiedQuery = util.promisify(connection.query);
 let currentUserId;
 
 const login = () => {
@@ -79,17 +81,17 @@ const init = () => {
       {
         type: "list",
         name: "init",
-        choices: ["BID", "POST", "QUIT"],
+        choices: ["BID on an item", "View POST menu", "QUIT"],
         message: "What would you like to do?",
       },
     ])
     .then(({ init }) => {
       switch (init) {
-        case "BID":
+        case "BID on an item":
           bid();
           break;
-        case "POST":
-          post();
+        case "View POST menu":
+          postMenu();
           break;
         default:
           connection.end();
@@ -98,7 +100,7 @@ const init = () => {
 };
 
 const bid = () => {
-  connection.query("SELECT * FROM items", (err, data) => {
+  connection.query("SELECT * FROM auctions", (err, data) => {
     if (err) throw err;
 
     inquirer
@@ -124,10 +126,11 @@ const bid = () => {
 
         if (bid > bidItem.highest_bid) {
           connection.query(
-            "UPDATE items SET ? WHERE ?",
+            "UPDATE auctions SET ? WHERE ?",
             [
               {
                 highest_bid: bid,
+                highest_bidder_id: currentUserId,
               },
               {
                 id: bidItemId,
@@ -147,6 +150,150 @@ const bid = () => {
   });
 };
 
+const getUserAuctionArray = async () => {
+  const userAuctions = await connection.promisifiedQuery(
+    "SELECT * FROM auctions WHERE created_by_id=?",
+    [currentUserId]
+  );
+
+  return userAuctions.map((auction) => {
+    return {
+      name: auction.item_description,
+      value: auction.id,
+    };
+  });
+};
+
+const closeAuction = async () => {
+  const auctionArray = await getUserAuctionArray();
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "auctionId",
+        choices: auctionArray,
+        message: "Which auction would you like to close?",
+      },
+    ])
+    .then(({ auctionId }) => {
+      connection.query(
+        "UPDATE auctions SET ? WHERE ?",
+        [
+          {
+            is_auction_open: false,
+          },
+          {
+            id: auctionId,
+          },
+        ],
+        (err, data) => {
+          if (err) throw err;
+          connection.query(
+            "SELECT item_description FROM auctions WHERE id = ?",
+            [auctionId],
+            (err, data) => {
+              if (err) throw err;
+              console.log(
+                `\nYour ${data[0].item_description} auction is now closed.\n`
+              );
+              init();
+            }
+          );
+        }
+      );
+    });
+};
+
+const editAuction = async () => {
+  const auctionArray = await getUserAuctionArray();
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "auctionId",
+        choices: auctionArray,
+        message: "Which auction would you like to modify?",
+      },
+      {
+        type: "list",
+        name: "column",
+        choices: [
+          { name: "Description", value: "item_description" },
+          { name: "Starting Bid", value: "starting_bid" },
+        ],
+        message:
+          "Would you like to change the description or the starting bid?",
+      },
+      {
+        type: "input",
+        name: "value",
+        message: "What would you like to change it to?",
+      },
+    ])
+    .then(({ auctionId, column, value }) => {
+      connection.query(
+        "UPDATE auctions SET ? WHERE ?",
+        [
+          {
+            [column]: value,
+          },
+          {
+            id: auctionId,
+          },
+        ],
+        (err, data) => {
+          if (err) throw err;
+          connection.query(
+            "SELECT item_description, starting_bid FROM auctions WHERE id = ?",
+            [auctionId],
+            (err, data) => {
+              if (err) throw err;
+              console.log(
+                `\nYour auction has been updated to ${data[0].item_description} with a starting bid of ${data[0].starting_bid}.\n`
+              );
+              init();
+            }
+          );
+        }
+      );
+    });
+};
+
+const viewAuctions = () => {
+  connection.query(
+    `
+    SELECT auctions.id, item_description, starting_bid, highest_bid, is_auction_open, username 
+    FROM auctions LEFT JOIN users 
+    ON auctions.highest_bidder_id = users.id 
+    WHERE created_by_id = ?`,
+    [currentUserId],
+    (err, data) => {
+      if (err) throw err;
+      console.log("\nYour Auctions:\n");
+      data.forEach(
+        ({
+          id,
+          item_description,
+          starting_bid,
+          highest_bid,
+          is_auction_open,
+          username,
+        }) => {
+          console.log(
+            `${id} | Item: ${item_description} | Starting Bid: ${starting_bid} | Highest Bid: ${
+              username ? highest_bid : "No bids yet."
+            } by User: ${username ? username : "Pending"} | ${
+              is_auction_open ? "Open" : "Closed"
+            }`
+          );
+        }
+      );
+      console.log("\n");
+      init();
+    }
+  );
+};
+
 const post = () => {
   inquirer
     .prompt([
@@ -164,14 +311,57 @@ const post = () => {
     ])
     .then(({ description, bid }) => {
       connection.query(
-        "INSERT INTO items SET ?",
-        { item_description: description, highest_bid: bid },
+        "INSERT INTO auctions SET ?",
+        {
+          item_description: description,
+          starting_bid: bid,
+          highest_bid: bid,
+          created_by_id: currentUserId,
+        },
         (err, res) => {
           if (err) throw err;
           console.log(`${res.affectedRows} item(s) inserted!\n`);
           init();
         }
       );
+    });
+};
+
+const postMenu = () => {
+  const choiceArray = [
+    "View your auctions",
+    "Post a new auction",
+    "Modify an auction",
+    "Close an auction",
+  ];
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "postChoice",
+        choices: choiceArray,
+        message: "Please select what you would like to do.",
+      },
+    ])
+    .then(({ postChoice }) => {
+      const choiceIndex = choiceArray.indexOf(postChoice);
+
+      switch (choiceIndex) {
+        case 0:
+          viewAuctions();
+          break;
+        case 1:
+          post();
+          break;
+        case 2:
+          editAuction();
+          break;
+        case 3:
+          closeAuction();
+          break;
+        default:
+          postMenu();
+      }
     });
 };
 
